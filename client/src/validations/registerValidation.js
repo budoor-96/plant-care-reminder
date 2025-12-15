24c0941050d756ffcd73e7465136bf56920712ff
Shatha001 <16j20127@utas.edu.om>
2025-12-15 21:10:10 +0400

    Update registerValidation.js

--- client/src/validations/registerValidation.js
+++ client/src/validations/registerValidation.js
@@ -1,16 +1,3 @@
-// import * as yup from "yup"
-
-// const regFormValidationSchema = yup.object().shape(
-//     {
-//         fullName : yup.string().required("Name cant be empty!"),
-//         dob : yup.string().required("Date of Birth cant be empty!"),
-//         email : yup.string().required("Email cant be empty!").email("Enter a valid email!"),
-//         pwd : yup.string().required("Cant be empty!").min(6, "Minimum 6 characters!"),
-//         cnfPwd : yup.string().required("Cant be empty!").oneOf([yup.ref("pwd"), null], "Passwords do not match!")
-//     }
-// )
-
-// export default regFormValidationSchema
 
 import * as yup from "yup";
 
@@ -34,3 +21,4 @@ const regFormValidationSchema = yup.object().shape({
 });
 
 export default regFormValidationSchema;
+
